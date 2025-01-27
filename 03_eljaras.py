3b320bec7888a378dc848273184a9e33850c6f7f
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2025-01-27 08:46:39 +0100

    1-3 eljaras

---
+++ 03_eljaras.py
@@ -0,0 +1,9 @@
+def osszegzo(lista):
+        # osszesen = 0
+        # for szam in lista:
+        #     osszesen += szam
+        osszeadva = sum(lista)
+        print('A listában lévő számok összege: ', osszeadva)
+
+szamok = [3, 5, 19, 11, 17, 1]
+osszegzo(szamok)
