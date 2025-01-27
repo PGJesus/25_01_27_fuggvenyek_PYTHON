cc1490810901b3c1663353cbbe3216678728e27c
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2025-01-27 09:22:50 +0100

    4-5 hatokorok; sort & sorted

---
+++ 05_sort_vs_sorted.py
@@ -0,0 +1,12 @@
+L = [1, 5, 4, 2, 3]
+
+sorted_list = sorted(L)
+
+print(f"Original list: {L}")
+print(sorted_list)
+
+print(L)
+sorted_list_2 = L.sort()
+print(f"After sort: {L}")
+print(L)
+print(sorted_list_2)
